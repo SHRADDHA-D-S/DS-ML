f09426393a23531c833f9d786866fdfa381138f3
SHRADDHA-D-S <shraddhads1523@gmail.com>
2026-06-27 12:37:01 +0530

    Update README.md and update dataset reference in Feature_Engineering.py

--- Feature_Engineering.py
+++ Feature_Engineering.py
@@ -5,7 +5,7 @@ import os
 
 print("Understanding dataset")
 
-file_name = 'sales.csv'
+file_name = 'data.csv'
 if not os.path.exists(file_name):
     print(f"error: {file_name} is not found")
     exit()
